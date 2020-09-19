09c770f45d799e39c34d5e51237be4ba54750091
Thiago Fernandes <thiago@spellbook.com.br>
2020-09-19 19:12:08 -0300

    removing fonts

--- src/components/template/index.js
+++ src/components/template/index.js
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react'
-import { TemplateContext } from '../../components/template/context'
-import { View } from './style'
+import React from "react";
+// import React, { useContext } from 'react'
+// import { TemplateContext } from '../../components/template/context'
+import { View } from "./style";
 
 export const Template = props => {
-  const { isMobile } = useContext(TemplateContext)
+  // const { isMobile } = useContext(TemplateContext)
 
-  return <View>{isMobile ? 'Isso é Boilerplate mobile' : props.children}</View>
-}
+  // return <View>{isMobile ? 'Isso é Boilerplate mobile' : props.children}</View>
+  return <View>{props.children}</View>;
+};
