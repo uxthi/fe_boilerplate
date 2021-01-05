112b15d198a318c6cac38189faa40a9e21f6dd92
uxthi <thiago@spellbook.com.br>
2021-01-05 12:09:15 -0300

    remove useless config

--- src/services/api.js
+++ src/services/api.js
@@ -7,31 +7,33 @@ const api = axios.create({
 
 export const logout = () => {
   localStorage.clear()
+  // Send to a custom page.
   window.location.href = ''
 }
 
-// api.interceptors.request.use(
-//   config => {
-//     const token = localStorage.getItem('token')
-//     if (token) {
-//       config.headers.Authorization = token
-//     } else {
-//       logout()
-//     }
+// Check if there is a valid token on localstorage and logout if there isn't.
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = token
+    } else {
+      logout()
+    }
+    return config
+  },
+  error => {
+    Promise.reject(error)
+  }
+)
 
-//     return config
-//   },
-//   error => {
-//     Promise.reject(error)
-//   }
-// )
-
-// api.interceptors.response.use(
-//   response => response,
-//   error => {
-//     if (error.response.status === 401) {
-//       logout()
-//     }
-//     return Promise.reject(error)
-//   }
-// )
+// If api response status is 401, logout user.
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response.status === 401) {
+      logout()
+    }
+    return Promise.reject(error)
+  }
+)
